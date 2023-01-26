895fce7af6b7e2c51d449d35d384f6bef34703d9
MikuxII <mickaelpoupion@gmail.com>
2023-01-26 10:39:09 +0100

    Méthod récupérer tous les utilisateurs

---
+++ src/Controller/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/user", name="user_")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/all", name="all", methods={"GET"})
+     */
+    public function all(UserRepository $userRepo)
+    {
+        // On récupère la liste des articles
+        $users = $userRepo->findAll();
+
+        // On spécifie qu'on utilise l'encodeur JSON
+        $encoders = [new JsonEncoder()];
+
+        // On instancie le "normaliseur" pour convertir la collection en tableau
+        $normalizers = [new ObjectNormalizer()];
+
+        // On instancie le convertisseur
+        $serializer = new Serializer($normalizers, $encoders);
+
+        // On convertit en json
+        $jsonContent = $serializer->serialize($users, 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);
+
+        // On instancie la réponse
+        $response = new Response($jsonContent);
+
+        // On ajoute l'entête HTTP
+        $response->headers->set('Content-Type', 'application/json');
+
+        // On envoie la réponse
+        return $response;
+        }
+}
